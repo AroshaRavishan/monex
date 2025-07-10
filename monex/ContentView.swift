202c715273bc7e027e7f0728be4398a8c8680c64
AroshaRavishan <mgaravishan@gmail.com>
2025-07-10 20:07:27 +0530

    Initial Commit

---
+++ monex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  monex
+//
+//  Created by Arosha on 2025-07-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ monex/monexApp.swift
@@ -0,0 +1,17 @@
+//
+//  monexApp.swift
+//  monex
+//
+//  Created by Arosha on 2025-07-10.
+//
+
+import SwiftUI
+
+@main
+struct monexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
